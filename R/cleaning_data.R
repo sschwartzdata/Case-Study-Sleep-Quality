2762152d4542f9d7960ab2612543e02b0b0d0dff
sschwartzdata <s.schwartz502@gmail.com>
2021-12-22 21:06:57 +0000

    adding month and quarter columns

--- R/cleaning_data.R
+++ R/cleaning_data.R
@@ -4,5 +4,12 @@ install.packages("tidyverse")
 library(tidyverse)
 
 #Pull in data
-sleep_data <- read_csv("./data/sleep_data.csv")
+sleep_data <- read_csv(file = "data/sleep_data.csv")
 
+#added month column to sleep data
+sleep_data <- sleep_data %>%
+  add_column(sleep_month = months(sleep_data$sleep_date))
+
+#added quarter column
+sleep_data <- sleep_data %>%
+  add_column(sleep_quarter = quarters(sleep_data$sleep_date))
