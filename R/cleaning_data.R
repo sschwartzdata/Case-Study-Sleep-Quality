0acee3bee5634c583d736b2b136072f5551fe441
Sarah <94994663+sschwartzdata@users.noreply.github.com>
2021-12-29 11:53:04 -0800

    Update cleaning_data.R
    cleaned up some plots

--- R/cleaning_data.R
+++ R/cleaning_data.R
@@ -17,7 +17,7 @@ library(tseries)
 #Pull in data
 sleep_data <- read_csv(file = "Data/sleep_data.csv")
 
-
+head(sleep_data)
 
 #added month column to sleep data
 sleep_data <- sleep_data %>%
@@ -29,8 +29,7 @@ sleep_data <- sleep_data %>%
 
 #make a long format of the data
 sleep_data_long <- melt(sleep_data, id.vars = c("sleep_date", "sleep_month",
- "sleep_quarter", "duration_hrs", "total_toss_turns",
- "avg_percent_rem_hrs"))
+ "sleep_quarter", "duration_hrs", "total_toss_turns"))
 
 #checking to see if correctly formatted
 head(sleep_data_long)
@@ -40,12 +39,21 @@ head(sleep_data_long)
 ave_duration <- mean(sleep_data$duration_hrs)
 std_duration <- sqrt(var(sleep_data$duration_hrs))
 var_duration <- var(sleep_data$duration_hrs)
+
+ave_toss <- mean(sleep_data$total_toss_turns)
+std_toss <- sqrt(var(sleep_data$total_toss_turns))
+var_toss <- var(sleep_data$total_toss_turns)
 #stat data frame
 stats <- data.frame(Statistics=c("Average", "Standard Deviation", "Variance"),
-                    Results=c(round(ave_duration, digits = 2), round(std_duration,digits = 2), 
-                                  round(var_duration,digits = 2)))
+                    Results=c(round(ave_duration, digits = 2),
+                              round(std_duration,digits = 2),
+                              round(var_duration,digits = 2)))
+stats2 <- data.frame(Statistics=c("Average", "Standard Deviation", "Variance"),
+                    Results=c(round(ave_toss, digits = 2),
+                              round(std_toss,digits = 2),
+                              round(var_toss,digits = 2)))
 
-view(stats)
+view(stats2)
 
 #plotting a bar plot of sleep duration with different colors for the months
 sleep_data %>%
@@ -67,17 +75,39 @@ yfit <- yfit * diff(h$mids[1:2]) * length(dur)
 
 lines(xfit, yfit, col = "blue", lwd = 2)
 
-#stacked bar graph - need to fix it so that it is the average, not the total
+#!!!!!!!!!! WORKING ZONE !!!!!!!!!!
 #attempting to make a table that has the average sleep stage per month
-long_average <-
-  sleep_data_long[which(sleep_data_long$sleep_date > "2014-01-17")]
+df <- sleep_data_long %>% group_by(variable) %>% summarize(me = mean(value))
+ggplot(aes(x = sleep_data_long$sleep_month, y = me), data = df) +
+  geom_bar(position = "stack", stat = "identity")
+#!!!!!!!!!! END WORKING ZONE !!!!!!!!!!
+
+#stacked bar graph - need to fix it so that it is the average, not the total
+
+head(sleep_data_long)
+df[df$date >= "2020-12-22", ]
+long_average <- sleep_data_long[sleep_data_long$sleep_date > "2014-01-17", ]
 head(long_average)
 
-n <- ggplot(long_average, aes(fill = variable, x = sleep_month,
- y = value, color = variable))
-n + geom_bar(position = "stack", stat = "identity")
+labels=c("total_rem_hrs"="REM", "total_deep_hrs"="Deep", 
+         "total_light_hrs"="Light", "total_interpurt_hrs"="Interupted",
+         "total_unknown_hrs"="Unknown")
 
-# testing normatilty of the data
+n <- ggplot(long_average, aes(fill = variable, x = sleep_month,
+ y = value, color = variable)) + 
+  scale_color_discrete(labels = labels)
+n + geom_bar(position = "fill", stat = "identity") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5))
+  
+
+#!!!!!!!!!! WORKING ZONE !!!!!!!!!!
+#attempting to make a table that has the average sleep stage per month
+df <- sleep_data_long %>% group_by(variable) %>% summarize(me = mean(value))
+ggplot(aes(x = sleep_data_long$sleep_month, y = me), data = df) +
+  geom_bar(position = "stack", stat = "identity")
+#!!!!!!!!!! END WORKING ZONE !!!!!!!!!!
+
+# testing if sleep duration is normal
 qqnorm(sleep_data$duration_hrs)
 shapiro.test(sleep_data$duration_hrs)
 tseries::jarque.bera.test(sleep_data$duration_hrs)
